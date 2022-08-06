e423f60eeb121632a9c4f2839338e5945f433651
Naidanasuresh <96282431+Naidanasuresh@users.noreply.github.com>
2022-08-06 19:18:44 +0530

    n=int(input()) lst=list(map(int,input().split())) c=0 f=[] for i in range(n):     if lst[i]%2==0 and i%2==0:         c+=1 for i in lst:     if i%2==0:         f.append(i) if c>=len(f):     print(True) else:     print(False)

--- Strictly_EVEN.py
+++ Strictly_EVEN.py
@@ -1,14 +1,14 @@
 n=int(input())
 lst=list(map(int,input().split()))
-cnt=0
-c=[]
-for i in range(len(lst)):
+c=0
+f=[]
+for i in range(n):
     if lst[i]%2==0 and i%2==0:
-        cnt+=1
+        c+=1
 for i in lst:
     if i%2==0:
-        c.append(i)
-if cnt>=len(c):
+        f.append(i)
+if c>=len(f):
     print(True)
 else:
     print(False)
